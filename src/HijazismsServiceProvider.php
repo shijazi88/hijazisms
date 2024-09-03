00eedba2f80e81907fd40027efaae0b13b203b40
shijazi <mohamadsafwan.hijazi@gmail.com>
2024-09-03 17:50:09 +0300

    first commiwt

--- src/HijazismsServiceProvider.php
+++ src/HijazismsServiceProvider.php
@@ -8,23 +8,22 @@ class HijazismsServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        // Merge the configuration file
+        // Merge the package configuration with the application's configuration
         $this->mergeConfigFrom(__DIR__.'/../config/hijazisms.php', 'hijazisms');
 
         // Register the SmsManager service
         $this->app->singleton(SmsManager::class, function ($app) {
             return new SmsManager($app);
         });
-
     }
 
     public function boot()
     {
+        \Log::info('HijazismsServiceProvider boot method called.');
+
         // Publish the configuration file
         $this->publishes([
             __DIR__.'/../config/hijazisms.php' => config_path('hijazisms.php'),
-        ], 'hijazisms-config');
-
-
+        ], 'config');
     }
 }
